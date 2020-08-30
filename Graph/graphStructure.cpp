936f925da4d1b5a72bbed4888225913ec8e125f8
HopeCollector <cmw4187@icloud.com>
2020-08-30 16:39:14 +0800

    Update graphStructure.cpp
    构造函数写好了，因为是智能指针的，所以必须在初始化的时候就把内存分配完毕，不然后续用起来，指针指向的都是 0

--- Graph/graphStructure.cpp
+++ Graph/graphStructure.cpp
@@ -1,13 +1,37 @@
 ﻿#include <vector>
 #include <memory>
 
-class Graph {
-public:
+struct Graph {
     struct Node
     {
         using Ptr = std::shared_ptr<Node>;
-        int val;
+        int val, idx;
         std::vector<Ptr> parents, children;
     };
     
+    struct Edge
+    {
+        using Ptr = std::shared_ptr<Edge>;
+        Node::Ptr from, to;
+        int val;
+    };
+    
+    std::vector<Node::Ptr> nodes;
+    std::vector<Edge::Ptr> edges;
+
+    Graph(int nodeNum = 0, int edgeNum = 0)
+    : nodes(nodeNum)
+    , edges(edgeNum) {
+        for (size_t i = 0; i < nodeNum; i++)
+        {
+            nodes[i] = std::make_shared<Node>();
+            nodes[i]->idx = i;
+        }
+        
+        for (size_t i = 0; i < edgeNum; i++)
+        {
+            edges[i] = std::make_shared<Edge>();
+        }
+    }
+
 };
