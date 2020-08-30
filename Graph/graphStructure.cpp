41b02e82a16d3a5890e0484f7b94ca5f550f15a1
HopeCollector <cmw4187@icloud.com>
2020-08-30 17:14:55 +0800

    Update graphStructure.cpp
    现在可以增加节点和边了

--- Graph/graphStructure.cpp
+++ Graph/graphStructure.cpp
@@ -26,12 +26,39 @@ struct Graph {
         {
             nodes[i] = std::make_shared<Node>();
             nodes[i]->idx = i;
+            nodes[i]->val = 0;
         }
         
         for (size_t i = 0; i < edgeNum; i++)
         {
             edges[i] = std::make_shared<Edge>();
+            edges[i]->val = 0;
         }
     }
+
+    Node::Ptr addNode(int val = 0) {
+        int idx = 0;
+        if(!nodes.empty()) idx = nodes.back()->idx+1;
+        
+        nodes.push_back(std::make_shared<Node>());
+        auto ptr = nodes.back();
+        ptr->idx = idx;
+        ptr->val = val;
+
+        return ptr;
+    }
+
+    Edge::Ptr addEdge(Node::Ptr from, Node::Ptr to, int val = 0) {
+        edges.push_back(std::make_shared<Edge>());
+        auto ptr = edges.back();
+
+        ptr->from = from;
+        ptr->to = to;
+        ptr->val = val;
+
+        return ptr;
+    }
+
+    inline Edge::Ptr addEdge(int from, int to, int val = 0) {return addEdge(nodes[from], nodes[to], val);}
 
 };
