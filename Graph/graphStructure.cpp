f7f3b33f15361630b9f908a53700454d63d15158
HopeCollector <cmw4187@icloud.com>
2020-08-30 16:19:48 +0800

    start develop graph utlity
    做一些跟 图 有关的结构和算法，方便以后用，每次都手搓一个可太麻烦了

---
+++ Graph/graphStructure.cpp
@@ -0,0 +1,13 @@
+﻿#include <vector>
+#include <memory>
+
+class Graph {
+public:
+    struct Node
+    {
+        using Ptr = std::shared_ptr<Node>;
+        int val;
+        std::vector<Ptr> parents, children;
+    };
+    
+};
